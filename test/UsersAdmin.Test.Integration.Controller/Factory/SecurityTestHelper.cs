860467f169b05bffe7accdc8ecb196ff3c682377
sschemberger <sebastian.s@embluemail.com>
2020-08-20 07:44:57 -0300

    v0.7.32 - WebAppFactoryFixture refactor

---
+++ test/UsersAdmin.Test.Integration.Controller/Factory/SecurityTestHelper.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Tatisoft.UsersAdmin.Core.Model.System;
+using Tatisoft.UsersAdmin.Core.Model.User;
+using Tatisoft.UsersAdmin.Core.Security;
+
+
+namespace Tatisoft.UsersAdmin.Test.Integration.Controller.Factory
+{
+    public class SecurityTestHelper
+    {
+        public UserLoggedDto UserAdmin { get; private set; }
+        public UserLoggedDto UserSystemAdmin { get; private set; }
+        public UserLoggedDto UserUser { get; private set; }
+
+        private readonly ITokenProvider _tokenProvider;
+
+        private readonly RepositoryTestHelper _repoHelper;
+
+        public SecurityTestHelper(IServiceCollection services, RepositoryTestHelper repoHelper)
+        {
+            //var jwtConfig = _configuration.GetSection("JwtConfig").Get<JwtConfig>();
+            _tokenProvider = services.BuildServiceProvider().GetService<ITokenProvider>();
+            _repoHelper = repoHelper;
+            this.ConfigureAdminUser();
+            this.ConfigureSystemAdminUser();
+            this.ConfigureUserUser();
+        }
+
+        private void ConfigureAdminUser()
+        {
+            UserEntity userEntity = new UserEntity() { Id = "ADMIN", Name = "Administrator", IsAdmin = true, Email = "admin@test.com", Pass = "x" };
+            
+            Task.Run(() => _repoHelper.InsertEntity<UserEntity>(userEntity)).Wait();
+            this.UserAdmin = this.BuildUserLogged(userEntity, null);
+        }
+
+        private void ConfigureSystemAdminUser()
+        {
+            var userEntity = new UserEntity() { Id = "SYS_ADM", Name = "System Admin", IsAdmin = false, Email = "admin@test.com", Pass = "x" };
+            var systemEntity = new SystemEntity() { Id = "TEST_SYSTEM_ADM", Name = "System for SystemAdmin" };
+            var userSystemEntity = new UserSystemEntity
+            {
+                SystemId = systemEntity.Id,
+                System = systemEntity,
+                UserId = userEntity.Id,
+                User = userEntity,
+                Role = UserRole.SystemAdmin
+            };
+            
+            Task.Run(() => _repoHelper.InsertEntity<UserSystemEntity>(userSystemEntity)).Wait();
+            this.UserSystemAdmin = this.BuildUserLogged(userEntity, systemEntity.Id);
+        }
+
+        private void ConfigureUserUser()
+        {
+            var userEntity = new UserEntity() { Id = "USER", Name = "System User", IsAdmin = false, Email = "user@test.com", Pass = "x" };
+            var systemEntity = new SystemEntity() { Id = "TEST_SYSTEM_USER", Name = "System for User" };
+            var userSystemEntity = new UserSystemEntity
+            {
+                SystemId = systemEntity.Id,
+                System = systemEntity,
+                UserId = userEntity.Id,
+                User = userEntity,
+                Role = UserRole.User
+            };
+
+            Task.Run(() => _repoHelper.InsertEntity<UserSystemEntity>(userSystemEntity)).Wait();
+            this.UserUser = this.BuildUserLogged(userEntity, systemEntity.Id);
+        }
+
+        private UserLoggedDto BuildUserLogged(UserEntity userEntity, string systemId)
+        {
+            var user = _repoHelper.MapperInstance.Map<UserLoggedDto>(userEntity);
+            var tokenInfo = _tokenProvider.BuildToken(userEntity, systemId);
+            user.Token = tokenInfo.Token;
+            user.Role = tokenInfo.Role;
+            return user;
+        }
+    }
+}
--- test/UsersAdmin.Test.Integration.Controller/Factory/WebAppFactoryFixture.cs
+++ test/UsersAdmin.Test.Integration.Controller/Factory/WebAppFactoryFixture.cs
@@ -10,10 +10,7 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
-using Tatisoft.UsersAdmin.Core.Model.System;
-using Tatisoft.UsersAdmin.Core.Model.User;
 using Tatisoft.UsersAdmin.Core.Repositories;
-using Tatisoft.UsersAdmin.Core.Security;
 using Tatisoft.UsersAdmin.Test.Integration.Controller.Factory.DataBase;
 
 namespace Tatisoft.UsersAdmin.Test.Integration.Controller.Factory
@@ -22,20 +19,20 @@ namespace Tatisoft.UsersAdmin.Test.Integration.Controller.Factory
     {
         protected readonly string MEDIA_TYPE = "application/json";
         protected readonly Encoding ENCODING = Encoding.UTF8;
-        private readonly string _localConnectionStringName = "AuthDbLocalSql";
         public readonly string CONTENT_TYPE = "application/json; charset=utf-8";
 
+        private readonly string _localConnectionStringName = "AuthDbLocalSql";
+
+        private readonly string _authHeaderScheme = "Bearer";
+
         public IServiceScopeFactory ScopeFactory { get; private set; }
-        public UserLoggedDto UserAdmin { get; private set; }
-        public UserLoggedDto UserSystemAdmin { get; private set; }
-        public UserLoggedDto UserUser { get; private set; }
 
         public IMapper MapperInstance => _repoHelper.MapperInstance;
 
-        private ITokenProvider _tokenProvider;
         private IConfiguration _configuration;
         private IDbTestContext _dbContext;
         private RepositoryTestHelper _repoHelper;
+        private SecurityTestHelper _securityHelper;
 
         public WebAppFactoryFixture() : base()
         {
@@ -51,7 +48,7 @@ namespace Tatisoft.UsersAdmin.Test.Integration.Controller.Factory
                 this.SetUpDbContext(services);
                 this.ScopeFactory = services.BuildServiceProvider().GetService<IServiceScopeFactory>();
                 _repoHelper = new RepositoryTestHelper(this.ScopeFactory);
-                SetupSecurity(services);
+                _securityHelper = new SecurityTestHelper(services, _repoHelper);
             });
         }
 
@@ -80,52 +77,6 @@ namespace Tatisoft.UsersAdmin.Test.Integration.Controller.Factory
             }
         }
 
-        private void SetupSecurity(IServiceCollection services)
-        {
-            //Used to give the config value.
-            //var jwtConfig = _configuration.GetSection("JwtConfig").Get<JwtConfig>();
-            _tokenProvider = services.BuildServiceProvider().GetService<ITokenProvider>();
-
-            UserEntity userEntity = new UserEntity() { Id = "ADMIN", Name = "Administrator", IsAdmin = true, Email = "admin@test.com", Pass = "x" };
-            Task.Run(() => _repoHelper.InsertEntity<UserEntity>(userEntity)).Wait();
-            this.UserAdmin = _repoHelper.MapperInstance.Map<UserLoggedDto>(userEntity);
-            var tokenInfo = _tokenProvider.BuildToken(userEntity, null);
-            this.UserAdmin.Token = tokenInfo.Token;
-            this.UserAdmin.Role = tokenInfo.Role;
-
-            userEntity = new UserEntity() { Id = "SYS_ADM", Name = "System Admin", IsAdmin = false, Email = "admin@test.com", Pass = "x" };
-            var systemEntity = new SystemEntity() { Id = "TEST_SYSTEM_ADM", Name = "System for SystemAdmin" };
-            var userSystemEntity = new UserSystemEntity
-            {
-                SystemId = systemEntity.Id,
-                System = systemEntity,
-                UserId = userEntity.Id,
-                User = userEntity,
-                Role = UserRole.SystemAdmin
-            };
-            this.UserSystemAdmin = _repoHelper.MapperInstance.Map<UserLoggedDto>(userEntity);
-            Task.Run(() => _repoHelper.InsertEntity<UserSystemEntity>(userSystemEntity)).Wait();
-            tokenInfo = _tokenProvider.BuildToken(userEntity, systemEntity.Id);
-            this.UserSystemAdmin.Token = tokenInfo.Token;
-            this.UserSystemAdmin.Role = tokenInfo.Role;
-
-            userEntity = new UserEntity() { Id = "USER", Name = "System User", IsAdmin = false, Email = "user@test.com", Pass = "x" };
-            systemEntity = new SystemEntity() { Id = "TEST_SYSTEM_USER", Name = "System for User" };
-            userSystemEntity = new UserSystemEntity
-            {
-                SystemId = systemEntity.Id,
-                System = systemEntity,
-                UserId = userEntity.Id,
-                User = userEntity,
-                Role = UserRole.User
-            };
-            this.UserUser = _repoHelper.MapperInstance.Map<UserLoggedDto>(userEntity);
-            Task.Run(() => _repoHelper.InsertEntity<UserSystemEntity>(userSystemEntity)).Wait();
-            tokenInfo = _tokenProvider.BuildToken(userEntity, systemEntity.Id);
-            this.UserUser.Token = tokenInfo.Token;
-            this.UserUser.Role = tokenInfo.Role;
-        }
-
         public StringContent CreateMessageContent(object dto)
         {
             var jsonDto = JsonConvert.SerializeObject(dto);
@@ -143,21 +94,24 @@ namespace Tatisoft.UsersAdmin.Test.Integration.Controller.Factory
         public HttpClient CreateAuthenticatedAsAdminClient()
         {
             var client = this.CreateClient();
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", this.UserAdmin.Token);
+            client.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue(_authHeaderScheme, this._securityHelper.UserAdmin.Token);
             return client;
         }
 
         public HttpClient CreateAuthenticatedAsSystemAdminClient()
         {
             var client = this.CreateClient();
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", this.UserSystemAdmin.Token);
+            client.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue(_authHeaderScheme, this._securityHelper.UserSystemAdmin.Token);
             return client;
         }
 
         public HttpClient CreateAuthenticatedAsUserClient()
         {
             var client = this.CreateClient();
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", this.UserUser.Token);
+            client.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue(_authHeaderScheme, this._securityHelper.UserUser.Token);
             return client;
         }
 
